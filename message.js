4cfc1f03f03d0d6513be61fabb0cc5a8cceb963d
Higor Cavalcanti Silva <higorsilva07.hs@gmail.com>
2020-09-13 19:46:41 -0400

    Finish Site

---
+++ message.js
@@ -0,0 +1,20 @@
+var titles = [
+    "Freios",
+    "Injeção Eletrônica",
+    "Motor e Câmbio",
+    "Suspenção",
+    "Tróca de Óleo"
+];
+
+var descs = [
+    "Descrição Freio",
+    "Descrição Injeção Eletrônica",
+    "Descrição Motor e Câmbio",
+    "Descrição Suspenção",
+    "Descrição Troca de Óleo",
+]
+
+function message(n){
+    document.getElementById("desc").innerText = descs[n];
+    document.getElementById("title").innerText = titles[n];
+}
